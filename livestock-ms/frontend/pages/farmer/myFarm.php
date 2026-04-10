28f381a631787fad5acd65c3da9e5b09a25b1d3d
clncodertien <agangaustien@gmail.com>
2026-04-10 22:37:26 +0800

    commit

--- livestock-ms/frontend/pages/farmer/myFarm.php
+++ livestock-ms/frontend/pages/farmer/myFarm.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>AgriHub</title>
 
-    <link rel="stylesheet" href="/livestock-ms/frontend/css/main.css" />
-    <link rel="stylesheet" href="/livestock-ms/frontend/css/sidebar.css" />
+    <link rel="stylesheet" href="../../css/main.css">
+    <link rel="stylesheet" href="../../css/sidebar.css">
 
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
